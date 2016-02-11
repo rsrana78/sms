85c112a99c41daa946908d4ea781b56806ea5c61
rana_rashid <rsrana78@yahoo.com>
2016-02-11 12:33:26 +0500

    alpha

--- sms/src/main/java/com/ayp/sms/service/SectionsService.java
+++
@@ -1,5 +0,0 @@
-package com.ayp.sms.service;
-
-public interface SectionsService {
-
-}
--- sms/src/main/webapp/js/main.js
+++
@@ -1,5 +0,0 @@
-//Validate email address
-function validateEmail(email) {
-    var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
-    return re.test(email);
-}
